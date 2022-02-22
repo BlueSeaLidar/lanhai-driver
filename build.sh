64501382e03c7f8abe4093ff13cf6de3d2dab167
BlueSeaLidar <60593455+BlueSeaLidar@users.noreply.github.com>
2022-02-22 16:58:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 gcc -c -o ./bin/uart.o uart.c
 # g++ -o record record.cpp uart.o
 g++ -o ./bin/uart-demo parser.cpp user.cpp uart_demo.cpp ./bin/uart.o
-g++ -o ./bin/udp-demo parser.cpp user.cpp udp_demo.cpp ./bin/uart.o
+g++ -o ./bin/udp-demo parser.cpp user.cpp udp_demo.cpp ./bin/uart.o -lpthread
 g++ -o ./bin/tcp-demo parser.cpp user.cpp tcp_demo.cpp ./bin/uart.o
